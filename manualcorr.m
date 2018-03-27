8254a4f8c6df1360918b5015d9fc66b1fea9cf10
jinyeom <jinyeom95@gmail.com>
2018-03-26 21:00:50 -0500

    manual done

--- manualcorr.m
+++ manualcorr.m
@@ -1,12 +1,23 @@
 function [P1, P2] = manualcorr(I1, I2)
-% Get manually identified corresponding points from two views. P1 and P2 each 
-% refers to rows of (x, y) coordinates of points in each image I1 and I2.
+% Get manually identified corresponding points from two views.
 	[w1, h1] = size(I1);
 	[w2, h2] = size(I2);
 
-	% Show two images on the same figures in separate axes.
-	f = figure;
-	ax1 = axes('View 1', [0.0, 0.0, 0.45, 1.0]);
-	ax2 = axes('View 2', [0.55, 0.0, 1.0, 1.0]);
+	% Show two images on the same figures in separate subplots.
+	figure;
+	subplot(1, 2, 1);
+    imshow(I1);
+    subplot(1, 2, 2);
+    imshow(I2);
 
+    % Manually select points on two subplots.
+    % NOTE: the user MUST click on the first image, then on the second
+    % image in the corresponding position, and then again on the first
+    % image, etc.
+    [x, y] = ginput();
+    
+    P1 = [x(1:2:size(x)), y(1:2:size(y))];
+    P2 = [x(2:2:size(x)), y(2:2:size(y))];
+    
+    close;
 end
