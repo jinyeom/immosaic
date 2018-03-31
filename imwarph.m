8fc7938fca92e637b14d613a08b36bc210065134
jinyeom <jinyeom95@gmail.com>
2018-03-31 16:24:11 -0500

    progress

--- imwarph.m
+++ imwarph.m
@@ -1,42 +1,47 @@
-function W = imwarph(I, H)
+function W = imwarph(I, J, H)
 % Warp the input image I1 via homography with H.
-    m = size(I, 1);
-    n = size(I, 2);
+    im = size(I, 1);
+    in = size(I, 2);
+    jm = size(J, 1);
+    jn = size(J, 2);
 
-    % Bounding box for the input image.
-    % Corners are in clock-wise order.
-    B1 = [1, 1; 
-          n, 1; 
-          n, m; 
-          1, m];
-      
     % Map all pixel values to warped image.
-    [IX, IY] = meshgrid(1:n, 1:m); 
-    [JX, JY] = homography([IX(:), IY(:)], H);   
-    J = zeros(max(JY(:)) - min(JY(:)), max(JX(:)) - min(JX(:)), 3);
-    J(JX, JY, :) = I(IX, IY, :);
-    imshow(J);
+    [IX, IY] = meshgrid(1:in, 1:im); 
+    P = round(homography([IY(:), IX(:)], H));   
+    minX = min(P(:, 1));
+    maxX = max(P(:, 1));
+    minY = min(P(:, 2));
+    maxY = max(P(:, 2));
 
-    % Construct a bounding box for the warped original
-    % bounding box.
-    B2 = round(homography(B1, H));
-    minX = min(B2(:, 1));
-    maxX = max(B2(:, 1));
-    minY = min(B2(:, 2));
-    maxY = max(B2(:, 2));
-    imshow(I);
-    hold on;
-    plot([B2(:, 1); B2(1, 1)]', [B2(:, 2); B2(1, 2)]');
-    plot([minX, maxX, maxX, minX, minX], [minY, minY, maxY, maxY, minY]);
-    
-    [X, Y] = meshgrid(minX:maxX, minY:maxY);
-    W = zeros([size(X), 3]);
-    disp(size(W));
-    
-    W(:, :, 1) = interp2(double(I(:, :, 1)), X, Y);    
-    W(:, :, 2) = interp2(double(I(:, :, 2)), X, Y);
-    W(:, :, 3) = interp2(double(I(:, :, 3)), X, Y);
+    % Coordinates of destination image.
+    % This image should be able to contain both images.
+    minDX = min(1, minX);
+    maxDX = max(jn, maxX);
+    minDY = min(1, minY);
+    maxDY = max(jm, maxY);
+    W = zeros(int8(maxDY - minDY), int8(maxDX - minDX), 3);
+
+    [X, Y] = meshgrid(minDX:maxDX, minDY:maxDY);
+    DC = [X(:), Y(:)]; 			% Coordinates in the destination image.
+    SC = round(homography(DC, inv(H)));	% Coordinates in the source image
+    					% (inverse warping).
+
+    % Transfer pixel values for each point.
+    for i = 1:size(DC, 1)
+        dx = max(1, DC(i, 1) - minDX);
+        dy = max(1, DC(i, 2) - minDY);
+        sx = SC(i, 1);
+        sy = SC(i, 2);
+        if sx >= 1 && sx <= in && sy >= 1 && sy <= im
+            W(dy, dx, :) = I(sy, sx, :);
+        end
+    end
+
+    %W(:, :, 1) = interp2(double(W(:, :, 1)), IX, IY);    
+    %W(:, :, 2) = interp2(double(W(:, :, 2)), IX, IY);
+    %W(:, :, 3) = interp2(double(W(:, :, 3)), IX, IY);
     W(isnan(W)) = 0;
     W = W / 255;
     
+    imshow(W);
 end
