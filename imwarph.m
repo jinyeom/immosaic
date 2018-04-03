204d7946c23df39585c6beb085fe314c17df5451
jinyeom <jinyeom95@gmail.com>
2018-04-03 04:42:08 -0500

    progress

--- imwarph.m
+++ imwarph.m
@@ -7,24 +7,31 @@ function W = imwarph(I, J, H)
 
     % Map all pixel values to warped image.
     [IX, IY] = meshgrid(1:in, 1:im); 
-    P = round(homography([IY(:), IX(:)], H));   
-    minX = min(P(:, 1));
-    maxX = max(P(:, 1));
-    minY = min(P(:, 2));
-    maxY = max(P(:, 2));
+    P = round(homography([IX(:), IY(:)], H));   
 
     % Coordinates of destination image.
     % This image should be able to contain both images.
-    minDX = min(1, minX);
-    maxDX = max(jn, maxX);
-    minDY = min(1, minY);
-    maxDY = max(jm, maxY);
-    W = zeros(int8(maxDY - minDY), int8(maxDX - minDX), 3);
+    minDX = min(1, min(P(:, 1)));
+    maxDX = max(jn, max(P(:, 1)));
+    minDY = min(1, min(P(:, 2)));
+    maxDY = max(jm, max(P(:, 2)));
+    W = zeros(int32(maxDY - minDY), int32(maxDX - minDX), 3);
+    
+    % First transfer the base image.
+    % This is a part of the "stitching the images". It's easier to put the
+    % base image down first, than doing so after warping the other image.
+    for y = 1:jm
+        for x = 1:jn
+            wx = min(max(x - minDX, 1), size(W, 2));
+            wy = min(max(y - minDY, 1), size(W, 1));
+            W(wy, wx, :) = J(y, x, :);
+        end
+    end
 
     [X, Y] = meshgrid(minDX:maxDX, minDY:maxDY);
-    DC = [X(:), Y(:)]; 			% Coordinates in the destination image.
+    DC = [X(:), Y(:)]; % Coordinates in the destination image.
     SC = round(homography(DC, inv(H)));	% Coordinates in the source image
-    					% (inverse warping).
+                                        % (inverse warping).
 
     % Transfer pixel values for each point.
     for i = 1:size(DC, 1)
@@ -37,6 +44,8 @@ function W = imwarph(I, J, H)
         end
     end
 
+    % This function doensn't seem to be necessary...
+    % Weird, but the result looks good enough I think.
     %W(:, :, 1) = interp2(double(W(:, :, 1)), IX, IY);    
     %W(:, :, 2) = interp2(double(W(:, :, 2)), IX, IY);
     %W(:, :, 3) = interp2(double(W(:, :, 3)), IX, IY);
