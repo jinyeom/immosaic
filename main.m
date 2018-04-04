676cb7688d12a63d28b5e77e6f7d006e2174029b
jinyeom <jinyeom95@gmail.com>
2018-04-03 22:45:23 -0500

    progress

--- main.m
+++ main.m
@@ -6,10 +6,32 @@
 % I = imread('uttower1.JPG');
 % J = imread('uttower2.JPG');
 % [P1, P2] = manualcorr(I, J);
-% H = solveh(P1, P2);
 load('setup.mat');
+
+% Plot the corresponding points.
+fig1 = figure;
+subplot(1, 2, 1);
+imshow(I); hold on;
+plot(P1(:, 1), P1(:, 2), 'rx');
+subplot(1, 2, 2);
+imshow(J); hold on;
+plot(P2(:, 1), P2(:, 2), 'bx');
+
+% Compute homography matrix and verify.
+H = solveh(P1, P2);
+P3 = homography(P1, H);
+fig2 = figure;
+subplot(1, 2, 1);
+imshow(I); hold on;
+plot(P1(:, 1), P1(:, 2), 'rx');
+subplot(1, 2, 2);
+imshow(J); hold on;
+plot(P3(:, 1), P3(:, 2), 'bx');
+
+% Using the two images and the homography matrix, stitch the images
+% together.
 W = imwarph(I, J, H);
-figure;
+fig3 = figure;
 imshow(W);
 
 % Now for the lab images.
